4a8abe6d7bac6c281b8bf6dec4a1aec6c0979e76
gabrielbrand <gabrielbrandgo@gmail.com>
2025-10-05 16:50:35 -0400

    Pasta avaliacao removida

--- avaliacao/src/pages/home.vue
+++
@@ -1,8 +0,0 @@
-<script setup>
-import Header from '@/components/Header/index.vue';
-import Home from '@/components/Home/index.vue';
-</script>
-<template>
-    <Header />
-    <Home />
-</template>
--- avaliacao/src/pages/receita.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import Header from '@/components/Header/index.vue';
-import Receita from '@/components/Receita/index.vue';
-</script>
-
-<template>
-    <Header />
-    <Receita />
-</template>
